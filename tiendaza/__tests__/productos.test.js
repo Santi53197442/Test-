14734074311730efef8a652b7249ecca4856e65c
Santi53197442 <115590856+Santi53197442@users.noreply.github.com>
2024-11-08 10:41:15 -0300

    Update productos.test.js

--- tiendaza/__tests__/productos.test.js
+++ tiendaza/__tests__/productos.test.js
@@ -2,28 +2,103 @@ const filtrarProductos = require('../js/productos');
 
 // Escribir los tests debajo
 test('Array vacío de productos', () => {
-  expect(filtrarProductos([], 'calzado')).toEqual([]);
+  expect(filtrarProductos([], 'pantalones')).toEqual([]);
 });
 
 test('Categoría no existente', () => {
-  const productos = [{ nombre: 'Camisa', categoria: 'ropa' }];
-  expect(filtrarProductos(productos, 'calzado')).toEqual([]);
+  const productos = [{
+    "id": "camiseta-08",
+    "titulo": "Remera 8",
+    "imagen": "./img/remera-08.jpg",
+    "categoria": "remeras",
+    "precio": 1000
+  }];
+  expect(filtrarProductos(productos, 'sombreros')).toEqual([]);
 });
 
-test('Categoría con coincidencias', () => {
+test('Categoría con coincidencias en dos', () => {
   const productos = [
-    { nombre: 'Zapatillas', categoria: 'calzado' },
-    { nombre: 'Sandalias', categoria: 'calzado' },
-    { nombre: 'Camisa', categoria: 'ropa' }
-  ];
-  expect(filtrarProductos(productos, 'calzado')).toEqual([
-    { nombre: 'Zapatillas', categoria: 'calzado' },
-    { nombre: 'Sandalias', categoria: 'calzado' }
+    {
+      "id": "pantalon-05",
+      "titulo": "Pantalón 05",
+      "imagen": "./img/pantalon-05.png",
+      "categoria": "pantalones",
+      "precio": 1000
+  },{
+    "id": "pantalon-06",
+    "titulo": "Pantalón 06",
+    "imagen": "./img/pantalon-06.png",
+    "categoria": "pantalones",
+    "precio": 1000
+},{
+  "id": "camiseta-08",
+  "titulo": "Remera 8",
+  "imagen": "./img/remera-08.jpg",
+  "categoria": "remeras",
+  "precio": 1000
+},];
+  expect(filtrarProductos(productos, 'pantalones')).toEqual([
+    {
+      "id": "pantalon-05",
+      "titulo": "Pantalón 05",
+      "imagen": "./img/pantalon-05.png",
+      "categoria": "pantalones",
+      "precio": 1000
+  },{
+    "id": "pantalon-06",
+    "titulo": "Pantalón 06",
+    "imagen": "./img/pantalon-06.png",
+    "categoria": "pantalones",
+    "precio": 1000
+}
   ]);
 });
 
 test('Categoría vacía', () => {
-  const productos = [{ nombre: 'Zapatillas', categoria: 'calzado' }];
+  const productos = [{
+    "id": "abrigo-05",
+    "titulo": "Campera 5",
+    "imagen": "./img/campera-05.jpg",
+    "categoria": "camperas",
+    "precio": 1000
+},];
   expect(filtrarProductos(productos, '')).toEqual([]);
 });
 
+test('Categoría con coincidencias en solo una', () => {
+  const productos = [
+    {
+      "id": "pantalon-05",
+      "titulo": "Pantalón 05",
+      "imagen": "./img/pantalon-05.png",
+      "categoria": "pantalones",
+      "precio": 1000
+  },{
+    "id": "pantalon-06",
+    "titulo": "Pantalón 06",
+    "imagen": "./img/pantalon-06.png",
+    "categoria": "pantalones",
+    "precio": 1000
+},{
+  "id": "camiseta-08",
+  "titulo": "Remera 8",
+  "imagen": "./img/remera-08.jpg",
+  "categoria": "remeras",
+  "precio": 1000
+},{
+  "id": "abrigo-01",
+  "titulo": "Campera 1",
+  "imagen": "./img/campera-01.jpg",
+  "categoria": "camperas",
+  "precio": 1000
+},];
+expect(filtrarProductos(productos, 'camperas')).toEqual([
+  {
+    "id": "abrigo-01",
+    "titulo": "Campera 1",
+    "imagen": "./img/campera-01.jpg",
+    "categoria": "camperas",
+    "precio": 1000
+  },
+]);
+});
